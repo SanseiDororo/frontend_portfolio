ec2f4858817b2eacd1507d148afca6692bb78275
SanseiDororo <philoscopes@gmail.com>
2024-02-20 15:13:24 +0100

    Renamed props for Dashbboard

--- src/components/DashSidebar.tsx
+++ src/components/DashSidebar.tsx
@@ -3,11 +3,11 @@ import sidebarlinks from './data/sidebarlinks'
 import { cn } from '@/lib/utils'
 import SidebarLinkItem from './SidebarLink'
 
-type DashboardProps = {
+type DashboardPageProps = {
   children?: React.ReactNode
 }
 
-const DashboardPage: React.FC<DashboardProps> = ({ children }) => {
+const DashboardPage: React.FC<DashboardPageProps> = ({ children }) => {
   return (
     <div className="flex">
       <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
